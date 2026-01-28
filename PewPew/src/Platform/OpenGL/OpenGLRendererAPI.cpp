68ed876165b7b09e615ac8b819ac68923252abc8
canberk <boom.canberk@gmail.com>
2026-01-28 12:42:36 +0100

    Add assimp prebuilt binaries

--- PewPew/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ PewPew/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -13,7 +13,7 @@ namespace PewPew {
 		glEnable(GL_DEPTH_TEST);
 		glDepthFunc(GL_LESS);
 	}
-	void OpenGLRendererAPI::SetClearColor(const glm::vec4& color)
+	void OpenGLRendererAPI::SetClearColor(const Vector4& color)
 	{
 		glClearColor(color.r, color.g, color.b, color.a);
 	}
@@ -23,7 +23,7 @@ namespace PewPew {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 
-	void OpenGLRendererAPI::DrawIndexed(const std::shared_ptr<VertexArray>& vertexArray)
+	void OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& vertexArray)
 	{
 		glDrawElements(GL_TRIANGLES, vertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
 	}
